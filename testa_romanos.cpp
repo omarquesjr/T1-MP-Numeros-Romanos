732f65580a519989d518e29ae11ce3fd5ba25ae4
Omarques <omarques.jrr@gmail.com>
2024-04-24 19:26:24 -0300

    alterações para adequação às regras do cpplint

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -15,11 +15,11 @@ TEST_CASE("Algarismos únicos", "[romanos]") {
 TEST_CASE("Algarismos inválidos", "[romanos]") {
     REQUIRE(romanos_para_decimal("G") == -1);
     REQUIRE(romanos_para_decimal("i") == -1);
-    REQUIRE( romanos_para_decimal("A") == -1);
-    REQUIRE( romanos_para_decimal("U") == -1);
-    REQUIRE( romanos_para_decimal("Z") == -1);
-    REQUIRE( romanos_para_decimal("b") == -1);
-    REQUIRE( romanos_para_decimal("m") == -1);
-    REQUIRE( romanos_para_decimal("O") == -1);
-    REQUIRE( romanos_para_decimal("T") == -1);
+    REQUIRE(romanos_para_decimal("A") == -1);
+    REQUIRE(romanos_para_decimal("U") == -1);
+    REQUIRE(romanos_para_decimal("Z") == -1);
+    REQUIRE(romanos_para_decimal("b") == -1);
+    REQUIRE(romanos_para_decimal("m") == -1);
+    REQUIRE(romanos_para_decimal("O") == -1);
+    REQUIRE(romanos_para_decimal("T") == -1);
 }
