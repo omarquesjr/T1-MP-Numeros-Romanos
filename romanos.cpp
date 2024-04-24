749358ea8666ad4fb779f9951e388cd2c9e56ed1
Omarques <omarques.jrr@gmail.com>
2024-04-24 19:19:52 -0300

    teste: algarismos únicos

--- romanos.cpp
+++ romanos.cpp
@@ -2,6 +2,11 @@
 int romanos_para_decimal(char const * num_romano) {
   if (num_romano[0] == 'I') return 1;
   else if (num_romano[0] == 'V') return 5;
+  else if (num_romano[0] == 'X') return 10;
+  else if (num_romano[0] == 'L') return 50;
+  else if (num_romano[0] == 'C') return 100;
+  else if (num_romano[0] == 'D') return 500;
+  else if (num_romano[0] == 'M') return 1000;
   else
     return -1;
 }
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -5,6 +5,11 @@
 TEST_CASE("Algarismos únicos", "[romanos]") {
     REQUIRE(romanos_para_decimal("I") == 1);
     REQUIRE(romanos_para_decimal("V") == 5);
+    REQUIRE(romanos_para_decimal("X") == 10);
+    REQUIRE(romanos_para_decimal("L") == 50);
+    REQUIRE(romanos_para_decimal("C") == 100);
+    REQUIRE(romanos_para_decimal("D") == 500);
+    REQUIRE(romanos_para_decimal("M") == 1000);  
 }
 
 TEST_CASE("Algarismos inválidos", "[romanos]") {
